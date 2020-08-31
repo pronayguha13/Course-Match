7a401ebbe1b3ad32fb9f46493970be45ddde5fb7
pronayguha <pronay.guha@aot.edu.in>
2020-08-31 16:15:15 +0530

    mongo connection established

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+//mongoose setup
+mongoose.connect(
+  `mongodb+srv://courseMatchBackend:${process.env.MONGODB_PWD}@course-match.ywvgn.mongodb.net/Course-Match?retryWrites=true&w=majority`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  () => {
+    console.log("MONGODB CONNECTED");
+  }
+);
 
 //Morgan Setup
 const morgan = require("morgan");
@@ -10,5 +23,12 @@ app.use(
     extended: false,
   })
 );
+
+app.use("/", (req, res, next) => {
+  res.send({
+    msg: "here is the home route",
+  });
+});
+
 app.use(express.json());
 module.exports = app;
