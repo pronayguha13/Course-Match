a80e8f6fb49b2fb95209c0d89ec620b98f7cf383
pronayguha <pronay.guha@aot.edu.in>
2020-10-02 14:16:05 +0530

    course model changed

--- backend/api/models/CourseModel.js
+++ backend/api/models/CourseModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const courseSchema = Schema({
   stream: { type: Schema.Types.ObjectId, ref: "Department" },
   semester: { type: Schema.Types.ObjectId, ref: "Semester" },
-  subject: { type: Schema.Types.ObjectId, ref: "Subject" },
+  subject: [{ type: Schema.Types.ObjectId, ref: "Subject" }],
 });
 
 module.exports = Course = mongoose.model("Course", courseSchema);
