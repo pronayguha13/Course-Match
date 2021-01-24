852f2bdeb902b306771b93488295e2b82f586e9d
pronayguha13 <pronay.guha@aot.edu.in>
2021-01-24 19:00:56 +0530

    password show/hide feature added in signin page

--- frontend/src/Components/Authentication/Signin/Auth.js
+++ frontend/src/Components/Authentication/Signin/Auth.js
@@ -2,14 +2,20 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import styles from "./auth.module.css";
-import { formValidationHandler } from "../../../helperMethods";
+import {
+  formValidationHandler,
+  changePasswordView,
+} from "../../../helperMethods";
 import { BASE_URL } from "../../../Context/AXIOS_BASE_URL";
 import { LoginContext } from "../../../Context/LoginContext";
-
 const Auth = () => {
   const [rollNumber, setRollNumber] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const [isRollActive, setIsRollActive] = useState(false);
+  const [rollError, setRollError] = useState(false);
+  const [isPwdActive, setIsPwdActive] = useState(false);
+  const [isHidden, setIsHidden] = useState(true);
   const history = useHistory();
   const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext);
 
@@ -17,9 +23,13 @@ const Auth = () => {
     const { name, value } = e.target;
     if (name === "roll_number") {
       const enteredRoll = value.trimLeft();
-      enteredRoll.length
-        ? setRollNumber(parseInt(enteredRoll))
-        : console.log("_onChangeHnadler -> enteredRoll", enteredRoll);
+      if (!isNaN(enteredRoll) && enteredRoll.length) {
+        setRollError(false);
+        setRollNumber(parseInt(enteredRoll));
+      } else {
+        setRollError(true);
+        setRollNumber("");
+      }
     } else {
       setPassword(value.trim());
     }
@@ -58,6 +68,7 @@ const Auth = () => {
           <label htmlFor="roll_number" className="col-sm-2 col-form-label">
             University Roll Number
           </label>
+
           <div className="col-sm-10">
             <input
               type="text"
@@ -65,6 +76,11 @@ const Auth = () => {
               name="roll_number"
               value={rollNumber}
               onChange={(e) => _onChangeHandler(e)}
+              style={
+                rollError
+                  ? { border: "5px outset #D64933" }
+                  : { border: "4px solid #ADEEE3" }
+              }
               required
               autoFocus
             />
@@ -76,13 +92,27 @@ const Auth = () => {
           </label>
           <div className="col-sm-10">
             <input
-              type="password"
+              type={isHidden ? "password" : "text"}
               className="form-control"
               id="inputPassword3"
               value={password}
               name="password"
               onChange={(e) => _onChangeHandler(e)}
+              onFocus={() => setIsPwdActive(true)}
+              onBlur={() => setIsPwdActive(false)}
               required
+            />
+            <img
+              src={
+                isHidden
+                  ? "/assets/images/icons/eye.png"
+                  : "/assets/images/icons/closed-eye.png"
+              }
+              alt="show-password"
+              className={
+                isPwdActive || password.length ? styles.showHidePassword : null
+              }
+              onClick={() => changePasswordView(isHidden, setIsHidden)}
             />
             {error !== null ? (
               <small id="passwordHelpBlock" className="form-text text-muted">
@@ -96,7 +126,7 @@ const Auth = () => {
             <button type="submit" className="btn btn-primary">
               Sign in
             </button>
-            <p style={{ marginTop: "20px" }}>
+            <p styles={{ marginTop: "20px" }}>
               {" "}
               if you dont have an account <Link to="/register">Sign Up</Link>
             </p>
