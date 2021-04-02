5d5241ac64f8c1dd57e6250c53e821c5f876961c
pronayguha13 <pronay.guha@aot.edu.in>
2021-04-02 17:58:20 +0530

    LoginForm seperated and designed

---
+++ frontend/src/Components/Forms/Login/LoginForm.jsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import { changePasswordView } from "../../../helperMethods";
+import styles from "./LoginForm.module.css";
+
+const LoginForm = ({ loginFormSubmitHandler }) => {
+  const [rollNumber, setRollNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [rollError, setRollError] = useState(false);
+  const [isHidden, setIsHidden] = useState(true);
+  const [rollFocus, setRollFocus] = useState(false);
+  const [passwordFocus, setPasswordFocus] = useState(false);
+  const _onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    if (name === "roll_number") {
+      const enteredRoll = value.trimLeft();
+      if (!isNaN(enteredRoll) && enteredRoll.length) {
+        setRollError(false);
+        setRollNumber(parseInt(enteredRoll));
+      } else {
+        setRollError(true);
+        setRollNumber("");
+      }
+    } else {
+      setPassword(value.trim());
+    }
+  };
+
+  //formDataHandler() method for sending form data from LoginForm.jsx to Auth.jsx
+  const formDataHandler = (e) => {
+    e.preventDefault();
+    const formData = {
+      rollNumber: rollNumber,
+      password: password,
+    };
+    loginFormSubmitHandler(formData);
+  };
+
+  return (
+    <div className={styles.LoginFormContainer}>
+      <form className={styles.LoginForm} onSubmit={(e) => formDataHandler(e)}>
+        <div className={styles.inputSection}>
+          <div className={styles.inputDiv}>
+            <span
+              className={
+                rollNumber || rollNumber.length || rollFocus
+                  ? styles.smallRollNumberPlaceholderText
+                  : styles.rollNumberPlaceholderText
+              }
+            >
+              <p> Enter Roll Number</p>
+            </span>
+            <input
+              type="text"
+              className={styles.RollNumberInput}
+              name="roll_number"
+              value={rollNumber}
+              onChange={(e) => _onChangeHandler(e)}
+              onFocus={() => {
+                setRollFocus(true);
+              }}
+              onBlur={() => {
+                setRollFocus(false);
+              }}
+              required
+            />
+          </div>
+          <div className={styles.inputDiv}>
+            <span
+              className={
+                passwordFocus || password.length
+                  ? styles.smallPasswordPlaceholderText
+                  : styles.passwordPlaceholderText
+              }
+            >
+              <p> Enter Password</p>
+            </span>
+            <input
+              type={isHidden ? "password" : "text"}
+              className={styles.PasswordInput}
+              id="inputPassword3"
+              value={password}
+              name="password"
+              onChange={(e) => _onChangeHandler(e)}
+              onFocus={() => setPasswordFocus(true)}
+              onBlur={() => setPasswordFocus(false)}
+              required
+            />
+            {password.length ? (
+              <img
+                src={
+                  isHidden
+                    ? "/assets/images/icons/eye.png"
+                    : "/assets/images/icons/closed-eye.png"
+                }
+                alt="show-password"
+                className={styles.showHidePassword}
+                onClick={() => changePasswordView(isHidden, setIsHidden)}
+              />
+            ) : null}
+          </div>
+        </div>
+        <div className={styles.SignInButtonContainer}>
+          <button type="submit" className="btn btn-primary">
+            Sign in
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
--- frontend/src/Containers/Authentication/Signin/Auth.jsx
+++ frontend/src/Containers/Authentication/Signin/Auth.jsx
@@ -2,59 +2,58 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import styles from "./auth.module.css";
-import {
-  formValidationHandler,
-  changePasswordView,
-} from "../../../helperMethods";
+import { formValidationHandler } from "../../../helperMethods";
 import { BASE_URL } from "../../../Context/AXIOS_BASE_URL";
 import { LoginContext } from "../../../Context/LoginContext";
 import { DisplayContext } from "../../../Context/DisplayContext";
 import Loading from "../../../Components/Layout/Loading.jsx";
 import ErrorPage from "../../../Components/Layout/ErrorPage";
 import SuccessPage from "../../../Components/Layout/SuccessPage";
-
+import LoginForm from "../../../Components/Forms/Login/LoginForm";
 let pause;
 
 const Auth = () => {
-  const [rollNumber, setRollNumber] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const [rollError, setRollError] = useState(false);
-  const [isPwdActive, setIsPwdActive] = useState(false);
-  const [isHidden, setIsHidden] = useState(true);
-  const [loading, setLoading] = useState(false);
-  const [showModal, setShowModal] = useState(false);
   const [isAuthSuccess, setIsAuthSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({});
   const history = useHistory();
-
   const { setIsLoggedIn } = useContext(LoginContext);
   const { themeSwitcher } = useContext(DisplayContext);
 
-  const _onChangeHandler = (e) => {
-    const { name, value } = e.target;
-    if (name === "roll_number") {
-      const enteredRoll = value.trimLeft();
-      if (!isNaN(enteredRoll) && enteredRoll.length) {
-        setRollError(false);
-        setRollNumber(parseInt(enteredRoll));
-      } else {
-        setRollError(true);
-        setRollNumber("");
-      }
-    } else {
-      setPassword(value.trim());
+  useEffect(() => {
+    themeSwitcher();
+    if (isAuthSuccess) {
+      setLoading(false);
+      pause = setTimeout(() => {
+        setIsAuthSuccess(false);
+        setIsLoggedIn(true);
+        history.push("/");
+      }, 2000);
+    } else if (error) {
+      setLoading(false);
+      pause = setTimeout(() => {
+        // clearField();
+        setError(false);
+      }, 1000);
     }
-  };
-
-  const _formSubmitHandler = (e) => {
-    e.preventDefault();
-    setLoading(true);
-
-    const formData = {
-      roll_number: rollNumber,
-      password: password,
+    return () => {
+      clearTimeout(pause);
     };
+  }, [
+    themeSwitcher,
+    history,
+    isAuthSuccess,
+    setIsLoggedIn,
+    error,
+    setLoading,
+    setError,
+  ]);
 
+  const loginFormSubmitHandler = (formData) => {
+    setFormData(formData);
+    setLoading(true);
     let authError = formValidationHandler(formData);
 
     if (authError !== false) {
@@ -75,40 +74,6 @@ const Auth = () => {
     }
   };
 
-  const clearField = () => {
-    setRollNumber("");
-    setPassword("");
-  };
-
-  useEffect(() => {
-    themeSwitcher();
-    if (isAuthSuccess) {
-      setLoading(false);
-      pause = setTimeout(() => {
-        setIsAuthSuccess(false);
-        setIsLoggedIn(true);
-        history.push("/");
-      }, 2000);
-    } else if (error) {
-      setLoading(false);
-      pause = setTimeout(() => {
-        clearField();
-        setError(false);
-      }, 1000);
-    }
-    return () => {
-      clearTimeout(pause);
-    };
-  }, [
-    themeSwitcher,
-    history,
-    isAuthSuccess,
-    setIsLoggedIn,
-    error,
-    setLoading,
-    setError,
-  ]);
-
   return (
     <div className={styles.SignIn}>
       <Loading loading={loading} />
@@ -123,71 +88,11 @@ const Auth = () => {
       ) : null}
 
       <div style={{ opacity: loading || error || isAuthSuccess ? 0.2 : 1 }}>
-        <h3>Sign in Page</h3>
-        <form onSubmit={(e) => _formSubmitHandler(e)}>
-          <div className={styles.formGroupRow}>
-            <label htmlFor="roll_number" className="col-sm-2 col-form-label">
-              University Roll Number
-            </label>
-
-            <div className={styles.inputDiv}>
-              <input
-                type="text"
-                className="form-control"
-                name="roll_number"
-                value={rollNumber}
-                onChange={(e) => _onChangeHandler(e)}
-                placeholder="Enter your University Roll number"
-                style={
-                  rollError
-                    ? { border: "5px outset #D64933" }
-                    : { border: "4px solid #ADEEE3" }
-                }
-                required
-                autoFocus
-              />
-            </div>
-          </div>
-          <div className={styles.formGroupRow}>
-            <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">
-              Password
-            </label>
-            <div className={styles.inputDiv}>
-              <input
-                type={isHidden ? "password" : "text"}
-                className="form-control"
-                id="inputPassword3"
-                value={password}
-                name="password"
-                onChange={(e) => _onChangeHandler(e)}
-                onFocus={() => setIsPwdActive(true)}
-                onBlur={() => setIsPwdActive(false)}
-                required
-              />
-              {isPwdActive || password.length ? (
-                <img
-                  src={
-                    isHidden
-                      ? "/assets/images/icons/eye.png"
-                      : "/assets/images/icons/closed-eye.png"
-                  }
-                  alt="show-password"
-                  className={styles.showHidePassword}
-                  onClick={() => changePasswordView(isHidden, setIsHidden)}
-                />
-              ) : null}
-            </div>
-          </div>
-          <div className={styles.SignInButtonContainer}>
-            <button type="submit" className="btn btn-primary">
-              Sign in
-            </button>
-            <p styles={{ marginTop: "20px" }}>
-              {" "}
-              if you dont have an account <Link to="/register">Sign Up</Link>
-            </p>
-          </div>
-        </form>
+        <LoginForm loginFormSubmitHandler={loginFormSubmitHandler} />
+        <p styles={{ marginTop: "20px" }}>
+          {" "}
+          if you dont have an account <Link to="/register">Sign Up</Link>
+        </p>
       </div>
     </div>
   );
