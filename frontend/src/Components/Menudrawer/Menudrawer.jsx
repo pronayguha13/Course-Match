00c9b9258f1f5f7159c1772d255b5328fc35022e
Pronay Guha <57461706+pronayguha13@users.noreply.github.com>
2023-09-29 23:54:02 +0530

    Refactored menu drawer logic to render menu items (#9)
    #8
    
    Co-authored-by: Pronay Guha <pronay.guha@thermofisher.com>

--- frontend/src/Components/Menudrawer/Menudrawer.jsx
+++ frontend/src/Components/Menudrawer/Menudrawer.jsx
@@ -1,25 +1,98 @@
-import React, { useContext, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
 
 import styles from "./Menudrawer.module.css";
 import { MenuDrawerContext } from "../../Context/MenuDrawer";
 import { LoginContext } from "Context/LoginContext";
 const Menudrawer = ({ isOpen }) => {
   const history = useHistory();
+  const location = useLocation();
+
+  const [menus, setMenus] = useState([]);
+
+  /*--------Method Declaration----------*/
+  const computeAvailableRoute = () => {
+    const availableRoutes = ["/", "/sign_in", "/register"];
+
+    const currentLocation = location.pathname;
+
+    const currentlyAvailableRoutes = availableRoutes.filter(
+      (route) => route !== currentLocation
+    );
+
+    return currentlyAvailableRoutes;
+  };
+
+  const generateMenuItems = () => {
+    let menuItems = menus.map((menu) => {
+      switch (menu) {
+        case "/":
+          return (
+            <span className={styles.MenuItem}>
+              <Link to="/">
+                <img src="/assets/images/icons/MenuDrawer/home_white_36dp.svg"></img>
+                <p>Home</p>
+              </Link>
+            </span>
+          );
+        case "/sign_in":
+          return (
+            <span className={styles.MenuItem}>
+              {!isLoggedIn ? (
+                <Link to="/sign_in">
+                  <img src="/assets/images/icons/MenuDrawer/login_white_36dp.svg"></img>
+                  <p>Log in</p>
+                </Link>
+              ) : (
+                <Link
+                  to="#"
+                  className={styles.logOutBtnContainer}
+                  onClick={() => {
+                    _logout();
+                  }}
+                >
+                  <img
+                    src="/assets/images/icons/MenuDrawer/logout_white_36dp.svg"
+                    alt="sign out"
+                  />
+                  <p>Log Out</p>
+                </Link>
+              )}
+            </span>
+          );
+        case "/register":
+          return (
+            <span className={styles.MenuItem}>
+              <Link to="/register">
+                <img src="/assets/images/icons/MenuDrawer/registration_white_36dp.svg"></img>
+                <p>Register</p>
+              </Link>
+            </span>
+          );
+      }
+    });
+
+    return menuItems;
+  };
+  /*--------Method DEclaration----------*/
   useEffect(() => {
     history.listen(() => {
       setIsMenuDrawerOpen(false);
     });
   }, [history]);
-  const { isMenuDrawerOpen, setIsMenuDrawerOpen } = useContext(
-    MenuDrawerContext
-  );
+  const { isMenuDrawerOpen, setIsMenuDrawerOpen } =
+    useContext(MenuDrawerContext);
   const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext);
 
   const _logout = () => {
     window.localStorage.clear();
     setIsLoggedIn(false);
   };
+
+  useEffect(() => {
+    setMenus(computeAvailableRoute());
+  }, [location.pathname]);
+
   return (
     <div
       className={
@@ -37,42 +110,7 @@ const Menudrawer = ({ isOpen }) => {
           alt="closeMenuDrawer"
         />
       </span>
-      <div className={styles.MenuItems}>
-        <span className={styles.MenuItem}>
-          <Link to="/">
-            <img src="/assets/images/icons/MenuDrawer/home_white_36dp.svg"></img>
-            <p>Home</p>
-          </Link>
-        </span>
-        <span className={styles.MenuItem}>
-          {!isLoggedIn ? (
-            <Link to="/sign_in">
-              <img src="/assets/images/icons/MenuDrawer/login_white_36dp.svg"></img>
-              <p>Log in</p>
-            </Link>
-          ) : (
-            <Link
-              to="#"
-              className={styles.logOutBtnContainer}
-              onClick={() => {
-                _logout();
-              }}
-            >
-              <img
-                src="/assets/images/icons/MenuDrawer/logout_white_36dp.svg"
-                alt="sign out"
-              />
-              <p>Log Out</p>
-            </Link>
-          )}
-        </span>
-        <span className={styles.MenuItem}>
-          <Link to="/register">
-            <img src="/assets/images/icons/MenuDrawer/registration_white_36dp.svg"></img>
-            <p>Register</p>
-          </Link>
-        </span>
-      </div>
+      <div className={styles.MenuItems}>{generateMenuItems()}</div>
     </div>
   );
 };
