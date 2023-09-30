185c2fb03fb6e4ca3832bda8493c5a04ed3aa4fe
debjit2001 <65007559+debjit2001@users.noreply.github.com>
2023-09-30 16:28:33 +0530

    Backend Initialization

---
+++ backend_new/course-match/src/test/java/com/project/course/match/CourseMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.course.match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
