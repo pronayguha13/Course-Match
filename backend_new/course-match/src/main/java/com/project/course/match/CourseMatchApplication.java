fa6c628c8c4bf2523b189e9d282304e3960fea97
debjit2001 <65007559+debjit2001@users.noreply.github.com>
2023-09-30 17:01:33 +0530

    Backend Initialization (#14)

---
+++ backend_new/course-match/src/main/java/com/project/course/match/CourseMatchApplication.java
@@ -0,0 +1,13 @@
+package com.project.course.match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMatchApplication.class, args);
+	}
+
+}
